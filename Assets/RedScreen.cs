a9b7ab2b49bd16734c5a3ff58127298a2ce88007
mvanspaendonck <michiel_v_s@hotmail.com>
2017-12-07 15:31:29 +0100

    Merged with Path

---
+++ Assets/RedScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedScreen : MonoBehaviour
+{
+
+    public GameObject redCanvas;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnPath.onPath)
+        {
+            redCanvas.SetActive(false);
+        }
+        else
+        {
+            redCanvas.SetActive(true);
+        }
+    }
+}
