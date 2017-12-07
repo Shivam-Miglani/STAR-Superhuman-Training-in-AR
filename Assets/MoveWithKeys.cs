1b9c129852c8f7df2252af25b84df0fe44a02084
Marie Kegeleers <mariekegeleers@wlan-145-94-231-60.wlan.tudelft.nl>
2017-12-07 12:47:19 +0100

    Path-lava detection

---
+++ Assets/MoveWithKeys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithKeys : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			transform.position = new Vector3(transform.position.x ,transform.position.y, transform.position.z - speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			transform.position = new Vector3(transform.position.x ,transform.position.y, transform.position.z + speed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/OnPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPath : MonoBehaviour {
+	
+	public static bool onPath = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//RaycastHit hit;
+
+		if(Physics.Raycast (transform.position, transform.InverseTransformDirection(Vector3.down), 20)) {
+			if (!onPath) {
+				Debug.Log ("On path");
+			}
+			onPath = true;
+		}else{
+			if (onPath) {
+				Debug.Log ("Not on path");
+			}
+			onPath = false;
+		}
+	}
+}
