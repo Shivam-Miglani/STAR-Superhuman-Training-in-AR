dbd605dda12127b6bd1ac2b04ed51388d89ab3c0
Marie <m.kegeleers@student.tudelft.nl>
2018-01-11 15:31:11 +0100

    Health and Energy Pickups + Bars

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+    public GameObject EnergyObject;
+    public LayerMask EnergyMask;
+    public int MaxItemsSpawned; // Maximum number of Energy pickups at the same time available in-game.
+    public const int MaxEnergy = 100;
+    public int CurrentEnergy;
+    public int EnergyPickupWorth = 25;
+    public float EnergyAlpha = 0.4f;
+    public int ScannerEnergyRate = 1;
+    public int RegenRate = 1;
+    public bool EnergyRegeneration = true;
+	public static bool MapPlaced = false;
+	public RectTransform healthBar;
+
+	// textures
+	public Texture2D EnergyBackground; // back segment
+    public Texture2D EnergyForeground; // front segment
+    public GUIStyle HUDSkin; // Styles up the health integer
+
+    //values   
+    private float EnergyBarWidth; //a value for creating the health bar size
+    private float myFloat; // an empty float value to affect drainage speed
+
+    private GameObject[] path;
+    private List<GameObject> EnergyObjects;
+    private float time = 0.0f;
+    private float timeRegen = 0.0f;
+    public float interpolationPeriod = 1.0f;
+    public float RegenInterpolationPeriod = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        path = GameObject.FindGameObjectsWithTag("Path");
+        EnergyObjects = new List<GameObject>();
+        CurrentEnergy = 50; //MaxEnergy;
+        EnergyBarWidth = 100f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (EnergyObjects.Count < MaxItemsSpawned && MapPlaced)
+        {
+            //TODO Prevent spawning on same part of path.
+            int rand = Random.Range(0, path.Length-1); //Get a random path index, make sure path has tag Path
+            SpawnEnergy(path[rand].transform.position);
+        }
+
+        time += Time.deltaTime;
+        timeRegen += Time.deltaTime;
+
+        if (XRay.XRayActive)
+        {
+            if (time >= interpolationPeriod)
+            {
+                time = 0.0f;
+                DecreaseEnergyScanner();
+            }
+
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.transform.position, Vector3.down, out hit, Mathf.Infinity, EnergyMask))
+			{
+				PickupEnergy(hit);
+			}
+		}
+        else if(EnergyRegeneration)
+        {
+            if (timeRegen >= RegenInterpolationPeriod)
+            {
+                timeRegen = 0.0f;
+                IncreaseEnergy(RegenRate);
+            }
+			
+		}
+		healthBar.sizeDelta = new Vector2(CurrentEnergy, healthBar.sizeDelta.y);
+	}
+
+    private void DecreaseEnergyScanner()
+    {
+        DecreaseEnergy(ScannerEnergyRate);
+    }
+
+    private void PickupEnergy(RaycastHit hit)
+    {
+        EnergyObjects.Remove(hit.transform.gameObject);
+        Destroy(hit.transform.gameObject);
+        IncreaseEnergy(EnergyPickupWorth);
+    }
+
+    private void SpawnEnergy(Vector3 PathLocation)
+    {
+        Vector3 loc = PathLocation;
+        loc.y = loc.y - 0.2f; //Spawn below path
+        GameObject g = Instantiate(EnergyObject, new Vector3(PathLocation.x, PathLocation.y - 0.1f, PathLocation.z), Quaternion.identity);
+        g.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        g.layer = LayerMask.NameToLayer("Energy");
+        g.AddComponent<BoxCollider>();
+		g.GetComponent<BoxCollider>().size = new Vector3(0.3f, 0.3f, 0.3f);
+        EnergyObjects.Add(g);
+    }
+
+    public bool DecreaseEnergy(int amount)
+    {
+        if(CurrentEnergy - amount < 0)
+        {
+            return false;
+        }
+
+        CurrentEnergy -= amount;
+        return true;
+    }
+
+    public void IncreaseEnergy(int amount)
+    {
+        CurrentEnergy += amount;
+        if (CurrentEnergy > MaxEnergy)
+        {
+            CurrentEnergy = MaxEnergy;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public const int MaxHealth = 100;
+    public int CurrentHealth;
+    public float HealthAlpha = 0.5f;
+
+    // textures
+    public Texture2D HealthBackground; // back segment
+    public Texture2D HealthForeground; // front segment
+    public GUIStyle HUDSkin; // Styles up the health integer
+	public RectTransform healthBar;
+
+	//values   
+	private float HealthBarWidth; //a value for creating the health bar size
+
+    private float time = 0.0f;
+    public float interpolationPeriod = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentHealth = MaxHealth; //MaxEnergy;
+        HealthBarWidth = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseHealth(1);
+		healthBar.sizeDelta = new Vector2(CurrentHealth, healthBar.sizeDelta.y);
+	}
+
+     public bool DecreaseHealth(int amount)
+    {
+        if (CurrentHealth - amount < 0)
+        {
+            return false;
+        }
+
+        CurrentHealth -= amount;
+        return true;
+    }
+
+
+
+    void OnGUI()
+    {
+        int posX = 10;
+        int posY = 27;
+        int height = 15;
+
+        float percentage = HealthBarWidth * ((float)CurrentHealth / (float)MaxHealth);
+        GUI.color = new Color(1, 1, 1, HealthAlpha);
+        GUI.DrawTexture(new Rect(posX, posY, (HealthBarWidth * 2), height), HealthBackground);
+        GUI.DrawTexture(new Rect(posX, posY, (percentage * 2), height), HealthForeground);
+
+    }
+}
